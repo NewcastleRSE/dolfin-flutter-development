6a45c0234d65e29a083ad418f03ba18d6d153e4d
Mike Simpson <mike.simpson@ncl.ac.uk>
2022-04-13 11:16:38 +0100

    Remove more old Task code

--- lib/data/repositories/firestore_crud.dart
+++ lib/data/repositories/firestore_crud.dart
@@ -1,30 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dolfin_flutter/data/models/child_model.dart';
-import 'package:dolfin_flutter/data/models/task_model.dart';
 
 class FireStoreCrud {
   FireStoreCrud();
 
   final _firestore = FirebaseFirestore.instance;
 
-  Future<void> addTask({required TaskModel task}) async {
-    var taskcollection = _firestore.collection('tasks');
-    await taskcollection.add(task.tojson());
-  }
-
   Future<void> addChild({required ChildModel child}) async {
     var childcollection = _firestore.collection('children');
     await childcollection.add(child.tojson());
-  }
-
-  Stream<List<TaskModel>> getTasks({required String mydate}) {
-    return _firestore
-        .collection('tasks')
-        .where('date', isEqualTo: mydate)
-        .snapshots(includeMetadataChanges: true)
-        .map((snapshor) => snapshor.docs
-            .map((doc) => TaskModel.fromjson(doc.data(), doc.id))
-            .toList());
   }
 
   Stream<List<ChildModel>> getChildren({required String parentID}) {
@@ -37,27 +21,6 @@ class FireStoreCrud {
             .toList());
   }
 
-  Future<void> updateTask(
-      {required String title,
-      note,
-      docid,
-      date,
-      starttime,
-      endtime,
-      required int reminder,
-      colorindex}) async {
-    var taskcollection = _firestore.collection('tasks');
-    await taskcollection.doc(docid).update({
-      'title': title,
-      'note': note,
-      'date': date,
-      'starttime': starttime,
-      'endtime': endtime,
-      'reminder': reminder,
-      'colorindex': colorindex,
-    });
-  }
-
   Future<void> updateChild({
     required String name,
     dob,
@@ -68,11 +31,6 @@ class FireStoreCrud {
       'name': name,
       'dob': dob,
     });
-  }
-
-  Future<void> deleteTask({required String docid}) async {
-    var taskcollection = _firestore.collection('tasks');
-    await taskcollection.doc(docid).delete();
   }
 
   Future<void> deleteChild({required String docid}) async {
